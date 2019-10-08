03bd60728adeb129691886751a870a5dae82fade
nigma97 <isuranga.17@cse.mrt.ac.lk>
2019-10-08 13:08:18 +0530

    ld1

--- lib/Datalayer/Database.dart
+++ lib/Datalayer/Database.dart
@@ -12,4 +12,17 @@ class Database {
     //TODO implement the backend connection for the function
     return list;
   }
+    static List<String> getUsers() {
+    List list = new List<String>();
+    int counter = 10;
+    int count = counter;
+    do {
+      int user = (count + 1) - counter;
+      list.add('$user User $user ');
+      counter--;
+    } while (counter > 0);
+
+    //TODO implement the backend connection for the function
+    return list;
+  }
 }
--- lib/UI/leaderboard.dart
+++ lib/UI/leaderboard.dart
@@ -1,22 +1,35 @@
+import 'package:edu_app/Datalayer/Database.dart';
 import 'package:flutter/material.dart';
+import 'package:edu_app/UI/user.dart';
+
+
 
 class LeaderboardPage extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    var size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Progress'),
+          title: Text('Leaderboard'),
         ),
         body: Container(
-          child: buildScreen(),
+          child: buildScreen(size),
         ),
       ),
     );
   }
-
-  Widget buildScreen() {
-    return Text('ui here');
-  }
 }
+ Widget buildScreen(size) {
+    List list = Database.getUsers();
+    return Container(
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, position) {
+          return buildUser(size, list, position);
+        },
+      ),
+    );
+  }
+
