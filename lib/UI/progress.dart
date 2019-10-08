91979f3edfdb1d19b25c27dff6c1355f7d402dcd
MithunW <mithun.17@cse.mrt.ac.lk>
2019-10-08 12:08:01 +0530

    progress bar

--- lib/UI/progress.dart
+++ lib/UI/progress.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class ProgressPage extends StatelessWidget {
   @override
@@ -24,109 +24,96 @@ class ProgressPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
-        title: new Text("My Progress"),
+        title: new Text("මගේ ප්‍රගතිය"),
       ),
       body: Center(
-        child: ListView(children: <Widget>[
-          new CircularPercentIndicator(
-            radius: 100.0,
-            lineWidth: 10.0,
-            percent: 0.8,
-            header: new Text("Mithun Wijethunga"),
-            center: new Icon(
-              Icons.person_pin,
-              size: 50.0,
-              color: Colors.blue,
-            ),
-            backgroundColor: Colors.grey,
-            progressColor: Colors.blue,
-          ),
-          new CircularPercentIndicator(
-            radius: 130.0,
-            animation: true,
-            animationDuration: 1200,
-            lineWidth: 15.0,
-            percent: 0.4,
-            center: new Text(
-              "40 hours",
-              style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-            ),
-            circularStrokeCap: CircularStrokeCap.butt,
-            backgroundColor: Colors.yellow,
-            progressColor: Colors.red,
-          ),
-          new CircularPercentIndicator(
-            radius: 120.0,
-            lineWidth: 13.0,
-            animation: true,
-            percent: 0.7,
-            center: new Text(
-              "70.0%",
-              style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-            ),
-            footer: new Text(
-              "Sales this week",
-              style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
-            ),
-            circularStrokeCap: CircularStrokeCap.round,
-            progressColor: Colors.purple,
-          ),
-          Padding(
-            padding: EdgeInsets.all(15.0),
-            child: new CircularPercentIndicator(
-              radius: 60.0,
-              lineWidth: 5.0,
-              percent: 1.0,
-              center: new Text("100%"),
-              progressColor: Colors.green,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(15.0),
-            child: new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new CircularPercentIndicator(
-                  radius: 45.0,
-                  lineWidth: 4.0,
-                  percent: 0.10,
-                  center: new Text("10%"),
-                  progressColor: Colors.red,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: new LinearPercentIndicator(
+                width: 140.0,
+                lineHeight: 14.0,
+                percent: 0.5,
+                center: Text(
+                  "50.0%",
+                  style: new TextStyle(fontSize: 12.0),
                 ),
-                new Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                ),
-                new CircularPercentIndicator(
-                  radius: 45.0,
-                  lineWidth: 4.0,
-                  percent: 0.30,
-                  center: new Text("30%"),
-                  progressColor: Colors.orange,
-                ),
-                new Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                ),
-                new CircularPercentIndicator(
-                  radius: 45.0,
-                  lineWidth: 4.0,
-                  percent: 0.60,
-                  center: new Text("60%"),
-                  progressColor: Colors.yellow,
-                ),
-                new Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                ),
-                new CircularPercentIndicator(
-                  radius: 45.0,
-                  lineWidth: 4.0,
-                  percent: 0.90,
-                  center: new Text("90%"),
-                  progressColor: Colors.green,
-                )
-              ],
+                trailing: Icon(Icons.mood),
+                linearStrokeCap: LinearStrokeCap.roundAll,
+                backgroundColor: Colors.grey,
+                progressColor: Colors.blue,
+              ),
             ),
-          )
-        ]),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: new LinearPercentIndicator(
+                width: 170.0,
+                animation: true,
+                animationDuration: 1000,
+                lineHeight: 20.0,
+                leading: new Text("left content"),
+                trailing: new Text("right content"),
+                percent: 0.2,
+                center: Text("20.0%"),
+                linearStrokeCap: LinearStrokeCap.butt,
+                progressColor: Colors.red,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: new LinearPercentIndicator(
+                width: MediaQuery.of(context).size.width - 50,
+                animation: true,
+                lineHeight: 20.0,
+                animationDuration: 2000,
+                percent: 0.9,
+                center: Text("90.0%"),
+                linearStrokeCap: LinearStrokeCap.roundAll,
+                progressColor: Colors.greenAccent,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: new LinearPercentIndicator(
+                width: MediaQuery.of(context).size.width - 50,
+                animation: true,
+                lineHeight: 20.0,
+                animationDuration: 2500,
+                percent: 0.8,
+                center: Text("80.0%"),
+                linearStrokeCap: LinearStrokeCap.roundAll,
+                progressColor: Colors.green,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Column(
+                children: <Widget>[
+                  new LinearPercentIndicator(
+                    width: 100.0,
+                    lineHeight: 8.0,
+                    percent: 0.2,
+                    progressColor: Colors.red,
+                  ),
+                  new LinearPercentIndicator(
+                    width: 100.0,
+                    lineHeight: 8.0,
+                    percent: 0.5,
+                    progressColor: Colors.orange,
+                  ),
+                  new LinearPercentIndicator(
+                    width: 100.0,
+                    lineHeight: 8.0,
+                    percent: 0.9,
+                    progressColor: Colors.blue,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
