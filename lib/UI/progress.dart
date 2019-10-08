d28ac5042471bfa9492668ab9b3970f994288833
MithunW <mithun.17@cse.mrt.ac.lk>
2019-10-08 12:24:49 +0530

    progress modified commented

--- lib/UI/progress.dart
+++ lib/UI/progress.dart
@@ -30,22 +30,22 @@ class ProgressPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(15.0),
-              child: new LinearPercentIndicator(
-                width: 140.0,
-                lineHeight: 14.0,
-                percent: 0.5,
-                center: Text(
-                  "50.0%",
-                  style: new TextStyle(fontSize: 12.0),
-                ),
-                trailing: Icon(Icons.mood),
-                linearStrokeCap: LinearStrokeCap.roundAll,
-                backgroundColor: Colors.grey,
-                progressColor: Colors.blue,
-              ),
-            ),
+            // Padding(
+            //   padding: EdgeInsets.all(15.0),
+            //   child: new LinearPercentIndicator(
+            //     width: 140.0,
+            //     lineHeight: 14.0,
+            //     percent: 0.5,
+            //     center: Text(
+            //       "50.0%",
+            //       style: new TextStyle(fontSize: 12.0),
+            //     ),
+            //     trailing: Icon(Icons.mood),
+            //     linearStrokeCap: LinearStrokeCap.roundAll,
+            //     backgroundColor: Colors.grey,
+            //     progressColor: Colors.blue,
+            //   ),
+            // ),
             Padding(
               padding: EdgeInsets.all(15.0),
               child: new LinearPercentIndicator(
@@ -87,34 +87,35 @@ class ProgressPage extends StatelessWidget {
                 progressColor: Colors.green,
               ),
             ),
-            Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Column(
-                children: <Widget>[
-                  new LinearPercentIndicator(
-                    width: 100.0,
-                    lineHeight: 8.0,
-                    percent: 0.2,
-                    progressColor: Colors.red,
-                  ),
-                  new LinearPercentIndicator(
-                    width: 100.0,
-                    lineHeight: 8.0,
-                    percent: 0.5,
-                    progressColor: Colors.orange,
-                  ),
-                  new LinearPercentIndicator(
-                    width: 100.0,
-                    lineHeight: 8.0,
-                    percent: 0.9,
-                    progressColor: Colors.blue,
-                  )
+            // Padding(
+            //   padding: EdgeInsets.all(15.0),
+            //   child: Column(
+            //     children: <Widget>[
+            //       new LinearPercentIndicator(
+            //         width: 100.0,
+            //         lineHeight: 8.0,
+            //         percent: 0.2,
+            //         progressColor: Colors.red,
+            //       ),
+            //       new LinearPercentIndicator(
+            //         width: 100.0,
+            //         lineHeight: 8.0,
+            //         percent: 0.5,
+            //         progressColor: Colors.orange,
+            //       ),
+            //       new LinearPercentIndicator(
+            //         width: 100.0,
+            //         lineHeight: 8.0,
+            //         percent: 0.9,
+            //         progressColor: Colors.blue,
+            //       )
                 ],
               ),
-            ),
-          ],
+            // ),
+          // ],
         ),
-      ),
+      // ),
     );
-  }
+  // }
+}
 }
